598021cc08241db3512e47febfd17bfa759f57d0
vboxuser <vboxuser@ubuntuvm.myguest.virtualbox.org>
2024-05-17 13:01:51 -0400

    eleventh  commit - added project plan section and added access to the index

---
+++ php/login.php
@@ -0,0 +1,19 @@
+<?php
+$submitted = !empty($_POST);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Handler</title>
+</head>
+<body>
+    <p>Form submitted? <?php echo (int) $submitted; ?></p>
+    <ul>
+        <li><b>Username:</b> <?php echo $_POST['username']; ?></li>
+        <li><b>Password:</b> <?php echo $_POST['password']; ?></li>
+
+    </ul>
+</body>
+</html>
